0b488f265710f9d12e03bd4f3df79edbf92804b9
CardwellInc <thebusinesssuite@gmail.com>
2022-03-03 23:21:32 -0800

    Update server.js

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ function checkNotAuthenticated(req, res, next) {
     next()
 }
 
-
+const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
