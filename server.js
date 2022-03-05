751e17eb59e2060ae0f0c0c2463d070ea0ed91f4
CardwellInc <thebusinesssuite@gmail.com>
2022-03-04 20:11:34 -0800

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
-const mongoose = require('mongoose')
 
 const initializePassport = require('./passport-config')
 initializePassport(
