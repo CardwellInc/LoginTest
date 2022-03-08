eae53a6b29b4be327fdee84ddf00843d0c502353
CardwellInc <thebusinesssuite@gmail.com>
2022-03-07 19:41:58 -0800

    Update server.js

--- server.js
+++ server.js
@@ -123,4 +123,8 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+const indexRouter = require('./routes/index')
+
+app.use('/', indexRouter)
+
 app.listen(process.env.PORT || 3000)
